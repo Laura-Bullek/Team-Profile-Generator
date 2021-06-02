4fe77add43f10bb95b09e7f0b2ac0f7bab919e2d
LB Vik <laura.bullek@gmail.com>
2021-06-01 22:18:26 -0400

    Made a new Manager info function to wrap all prompt functions into CreateCard function

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Intern = require('./lib/Intern');
 
 function createCard() {
 
+  // Manager prompt
+  function managerInfo() {
   return inquirer.prompt([
       {
           type: 'input',
@@ -29,10 +31,11 @@ function createCard() {
           message: 'What is their phone number?'
       }
 
-  ])}
+  ])
+  }
 
-console.log(createCard());
 
+// Intern prompts
 function internInfo() {
 
   return inquirer.prompt([
@@ -56,29 +59,50 @@ function internInfo() {
           name: 'internSchool',
           message: 'What school did the intern attend?'
       }
-  ])}
+  ])
+}
 
-  function engineerInfo() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'engineerName',
-            message: 'What is the name of the engineer?'
-        },
-        {
-            type: 'input',
-            name: 'engineerId',
-            message: 'Type in the ID number'
-        },
-        {
-            type: 'input',
-            name: 'engineerEmail',
-            message: 'What is their email?'
-        },
-        {
-            type: 'input',
-            name: 'engineerGithub',
-            message: 'What is the GitHub username of the engineer?'
-        }
-    ])}
-  
+// Engineer prompts
+function engineerInfo() {
+
+  return inquirer.prompt([
+      {
+          type: 'input',
+          name: 'engineerName',
+          message: 'What is the name of the engineer?'
+      },
+      {
+          type: 'input',
+          name: 'engineerId',
+          message: 'Type in the ID number'
+      },
+      {
+          type: 'input',
+          name: 'engineerEmail',
+          message: 'What is their email?'
+      },
+      {
+          type: 'input',
+          name: 'engineerGithub',
+          message: 'What is the GitHub username of the engineer?'
+      }
+  ])
+}
+
+// List generated 
+function addEmployee() {
+
+  return inquirer.prompt([
+      {
+          type: 'list',
+          name: 'addEmployee',
+          message: 'Would you like to add a new team member?',
+          choices: [
+              'Manager',
+              'Intern',
+              'Engineer',
+              'No'
+          ]
+      }])
+}
+}
